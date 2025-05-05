062a6dd365b87a482ba5d71415fb00003c6ccde1
Vita Gold <golvi1124@gmail.com>
2025-05-05 12:15:29 +0200

    Add project files.

---
+++ CandyShop/Program.cs
@@ -0,0 +1,45 @@
+﻿string title = "Mary's Candy Shop";
+string divide = "----------------------";
+DateTime dateTime = DateTime.Now;
+int daysSinceOpening = 1;
+decimal todaysProfit = 5.5m;
+bool targetAchieved = false;
+
+string menu = @"Choose one option:
+'V' to view products
+'A' to add a product
+'D' to delete a product
+'U' to update a product";
+
+Console.WriteLine($@"
+{title}
+{divide}
+Today's date: {dateTime}
+Days since opening: {daysSinceOpening}
+Today's profit: {todaysProfit:C}$
+Target achieved: {targetAchieved}
+{divide}
+{menu}
+");
+Console.ReadLine();
+
+var usersChoice = Console.ReadLine().Trim().ToUpper();
+
+switch (usersChoice)
+{
+    case "A":
+        Console.WriteLine("You chose to add a product.");
+        break;
+    case "V":
+        Console.WriteLine("You chose to view products.");
+        break;
+    case "D":
+        Console.WriteLine("You chose to delete a product.");
+        break;
+    case "U":
+        Console.WriteLine("You chose to update a product.");
+        break;
+    default:
+        Console.WriteLine("Invalid choice. Please try again.");
+        break;
+}
